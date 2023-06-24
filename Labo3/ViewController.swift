d78d4367dfad977c6bda7d48b942ebd359a8aa26
Yassine <peace.org.ca@gmail.com>
2023-06-24 12:54:06 -0400

    Initial Commit

---
+++ Labo3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Labo3
+//
+//  Created by Yassine Belkaousse (Étudiant) on 2023-06-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Labo3UITests/Labo3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Labo3UITestsLaunchTests.swift
+//  Labo3UITests
+//
+//  Created by Yassine Belkaousse (Étudiant) on 2023-06-24.
+//
+
+import XCTest
+
+class Labo3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
